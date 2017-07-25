a78899f68e0c60c549071c73d7c6ac790cb0268a
Jinwoo Hong <hjw7789@gmail.com>
2017-07-25 22:15:05 +0900

    change pull-request title of master

--- lib/hub_flow.rb
+++ lib/hub_flow.rb
@@ -4,10 +4,10 @@ module HubFlow
   def self.pull_request(message)
     branch_name = `git rev-parse --abbrev-ref HEAD`.chomp
 
-    if branch_name.start_with? 'hotfix'
-      system "hub pull-request -m \"#{message}\" -b master"
-    end
-
     system "hub pull-request -m \"#{message}\" -b develop"
+
+    if branch_name.start_with? 'hotfix'
+      system "hub pull-request -m \"#{'[DEPLOY] ' + message}\" -b master"
+    end
   end
 end
